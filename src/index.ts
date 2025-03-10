f9f91f4b164c3911a304229c0eb01232c5d6509c
Richard Essuman <ressuman001@gmail.com>
2025-03-10 17:35:06 +0000

    initialize backend with Express, TypeScript, and essential middleware

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+// Packages
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
+import http from "http";
+import compression from "compression";
+
+// Utilities
+
+// Database Connection
+
+dotenv.config();
+const port = process.env.PORT_NUMBER || 4200;
+
+const app = express();
+
+// Middlewares
+app.use(helmet());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+); // Configure CORS
+app.use(morgan("dev"));
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ extended: true, limit: "20mb" }));
+app.use(compression());
+app.use(cookieParser());
+
+// Routes
+
+// Error Handling Middleware
+// app.use(notFound);
+// app.use(errorHandler);
+
+const server = http.createServer(app);
+server.listen(port, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port: ${port}, and listening to requests at http://localhost:${port}`
+  );
+});
+
+export default server;
